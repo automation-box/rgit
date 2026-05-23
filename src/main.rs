665c9ae098b3cd9e9460c3ca45b7b5ca5bb73470
automation-box <261583692+automation-box@users.noreply.github.com>
2026-05-22 19:41:06 -0600

    Add init and help commands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
 use std::env;
 
+#[derive(Debug)]
+struct CliConfig {
+    postional_args: Vec<String>,
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    // Initialize program configuration
+    let mut config = CliConfig {
+        postional_args: Vec::new(),
+    };
+
+    // Turn arguments into an iterator
+    let mut args_iter = env::args();
+    args_iter.next();
+
+    while let Some(arg) = args_iter.next() {
+        match arg.as_str() {
+            "init" => {
+                println!("Initializing repository...")
+            }
+
+            "-h" | "--help" => {
+                println!("Rgit is mock implementation of git in Rust");
+                println!("Usage [FLAGS] [ARGS]")
+            }
+
+            // Save everything else as regular positional arguments
+            positional => config.postional_args.push(positional.to_string()),
+        }
+    }
 }
