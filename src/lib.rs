b3f75c9216936f66014901dc255c95a53bcb5ac6
automation-box <261583692+automation-box@users.noreply.github.com>
2026-05-26 01:33:10 -0600

    Automatically run command on invocation place and create refs and objects file

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use std::fs;
 use std::io::{self, Write};
 use std::path::PathBuf;
+use std::{env, fs};
 
 /// The distinct actions rgit can take.
 #[derive(Debug, PartialEq, Eq)]
@@ -45,15 +45,19 @@ where
 
 /// Handles UI feedback and actual execution.
 /// Accepting a writer allows us to test the console output of commands if needed
-pub fn execute_command<W>(command: &Command, base_dir: PathBuf, mut writer: W) -> io::Result<()>
+pub fn execute_command<W>(command: &Command, mut writer: W) -> io::Result<()>
 where
     W: Write,
 {
+    let base_dir = env::current_dir().unwrap_or_default();
     match command {
         Command::Init => {
             writeln!(writer, "Initializing repository...")?;
             let rgit_dir = base_dir.join(".rgit");
-            fs::create_dir(rgit_dir)?;
+
+            fs::create_dir(&rgit_dir)?;
+            fs::create_dir(&rgit_dir.join("objects"))?;
+            fs::create_dir(&rgit_dir.join("refs"))?;
         }
         Command::Help => {
             writeln!(writer, "Rgit is a mock implementation of git in Rust")?;
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,8 @@ fn main() {
     // Parsing is clean and independent of stdout
     let config = rgit::parse_args(env::args());
 
-    let current_dir = env::current_dir().unwrap_or_default();
-
     // Side-effects and execution happen here
-    if let Err(e) = rgit::execute_command(&config.command, current_dir, io::stdout()) {
+    if let Err(e) = rgit::execute_command(&config.command, io::stdout()) {
         eprintln!("Error executing command: {}", e);
     }
 }
